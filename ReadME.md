549053002df873ed6a7aaa25fa32fb0911a3a167
MazenTURK <100404112+MazenTurk201@users.noreply.github.com>
2024-10-20 03:39:11 +0300

    Add files via upload

---
+++ ReadME.md
@@ -0,0 +1,65 @@
+<p align="center">
+<img src="https://i.postimg.cc/3J6Ngk7C/Turk.png">
+</p>
+
+# About
+
+To run The tool just write [`tl`](https://github.com/MazenTurk201/TurkLine)
+
+
+<!-- My friend, I am not responsible for any misuse of the tool or abuse, and this is illegal. The goal of the tool is Android penetration testing for beginners in the field of "Pentration Testing". Thank you. -->
+
+
+## Discribe
+
+ * this tool to help some one to read data
+
+
+## Download
+ ```bash
+ git clone https://github.com/MazenTurk201/MT
+ ```
+ ```
+ cd MT
+ ```
+ ```
+ chmod +x *
+ ```
+
+## Run
+ ```bash
+ ./MT.sh or "turk"
+ ```
+## Syntax
+ ```bash
+ cd MT
+
+ -h       # for Help. 
+ -p       # To continue Exploiting Android.
+ -a       # for ADB Shell.
+ -w       # To continue Exploiting Windows.
+```
+
+## Notic
+```
+The Tools Use:
+
+  [+] Net-Tools
+  [+] Apache2-bin
+  [+] MetaSploit-Framework
+  [+] Beef
+  [+] Scrcpy
+  [+] Nmap
+  [+] Android Debug Bridge (ADB)
+  [+] Zenity
+  [+] 
+  [+]  
+```
+
+ # To report any problem
+
+ [-] [*WhatsApp*](https://wa.me/+201092130013?text=Hi+Turk)
+
+ [-] [*Instagram*](https://www.instagram.com/mazenturkk)
+
+ [-] [*Gmail*](https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKkVTRnqhcdnCKGrmdBNQfSLQrcjrDrrhRXjGpJlNsGLRjBPKpWTHBwbfpvzphhWZzprdvh)
---
+++ install.sh
@@ -0,0 +1,85 @@
+## Varyabol ##
+	file=$1
+    num=$2        
+	p="p"
+    hi=$num$p
+    path=`pwd`
+##############
+
+## Check Bin ##
+    check_bin(){
+        which tl > /dev/null 2>&1
+		    if [ "$?" -eq "1" ]; then
+		    sudo ln -s $path/install.sh /usr/local/bin/tl
+		    else {
+		       which
+		    }
+		    fi
+            }
+###############
+
+## Check Bin Termux ##
+    check_bin_termux(){
+        which tl > /dev/null 2>&1
+		    if [ "$?" -eq "1" ]; then
+		    ln -s $path/install.sh $PREFIX/bin/tl
+		    else {
+		       which
+		    }
+		    fi
+            }
+######################
+
+## Help ##
+	help(){
+		echo "usage:
+	tl                     # Help (tl -h)
+	tl example.txt         # to know how many lines in file
+	tl example.txt 3       # to display line number
+		"
+		}
+##########
+
+## Syntax ##
+	while getopts ':h|:H|' option; do 
+		case $option in
+			h|H) 
+			help
+			exit;;
+			\?) 
+			echo "Invaled Syntax....."
+			help
+			exit;;
+			esac
+			done
+############
+
+## TL ##
+	tl(){
+	if [ -f '/etc/arch-release' ]; then
+	    check_bin
+	else
+	    if [ "$OSTYPE" == 'linux-android' ]; then
+	        check_bin_termux
+	    else
+	        check_bin
+	    fi
+	fi
+	}
+########
+
+## Tool ##
+	tl
+	if [ -z $1 ];then
+	help
+	else {
+		if [ -z $2 ];then
+		wc -l $file | awk '{print $1}'
+		else {
+		cat $file | sed -n "$hi"
+		}
+		fi
+	}
+	fi
+##########
+# 2024/10/20 02:30
